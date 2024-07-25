37e243e0c1a6f2cf5a884b311ebd3f590a667e56
codemilli <hckrmoon@gmail.com>
2024-07-25 22:07:08 +0900

    update faq link

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,7 +1,7 @@
 const TICKET_LINK = 'https://booking.naver.com/booking/5/bizes/263881';
 const FE_GROUP_LINK = 'https://www.facebook.com/feconf.kr';
 const FAQ_LINK =
-  'https://www.notion.so/FAQ-198b5b664b9c457b8faadc1517888f94?pvs=4';
+  'https://neighborly-dracopelta-8f3.notion.site/FAQ-198b5b664b9c457b8faadc1517888f94';
 const EMAIL = 'feconf@googlegroups.com';
 
 export { TICKET_LINK, FE_GROUP_LINK, FAQ_LINK, EMAIL };
