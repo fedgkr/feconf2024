901c1389487d0f9c6e1a11d35397f618560e822c
Daybrush <younkue.choi@navercorp.com>
2024-07-25 21:05:25 +0900

    test

--- src/features/aurora/components/Aurora.tsx
+++ src/features/aurora/components/Aurora.tsx
@@ -20,7 +20,7 @@ if (typeof window !== 'undefined') {
     })
     .catch(error => console.error('Failed to load dat.GUI:', error));
 }
-
+//
 const vertexShader = `
   vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
   vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
